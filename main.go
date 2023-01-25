82ee59623dfecd26f0a00d948ad6caa0db378603
armzerpa <armando_zerpa@hotmail.com>
2023-01-25 21:15:41 +0100

    tls

--- main.go
+++ main.go
@@ -4,19 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var sslkey string = "./certs/server.key"
+var sslcert string = "./certs/server.crt"
+
 func main() {
 	r := gin.Default()
-
-	/*authorized := r.Group("/api", gin.BasicAuth(gin.Accounts{
-		"user1": "testdigibee",
-		"user2": "testaz",
-	}))*/
 
 	authorized := r.Group("/api")
 	authorized.GET("/ping", ping)
 
 	//r.Use(static.Serve("/", static.LocalFile("./views", true)))
-	r.Run()
+	r.RunTLS(":8080", sslcert, sslkey)
 }
 
 func ping(c *gin.Context) {
